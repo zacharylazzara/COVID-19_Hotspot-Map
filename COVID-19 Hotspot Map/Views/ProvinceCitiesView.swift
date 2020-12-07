910cd33db6090d2088342918890e0f14464b69a4
yuan wang <wang85@sheridancollege.ca>
2020-12-07 13:21:06 -0500

    data populated in the province cities

--- COVID-19 Hotspot Map/Views/ProvinceCitiesView.swift
+++ COVID-19 Hotspot Map/Views/ProvinceCitiesView.swift
@@ -31,4 +31,46 @@ struct ProvinceCitiesView: View {
             }
         }
     }
+    var body: some View {
+        VStack(spacing: 0.0) {
+            List {
+                ForEach(self.localityList.filter({ searchText.isEmpty ? true : $0.name!.contains(searchText) }), id: \.self) { locality in
+                    ZStack {
+                        VStack(alignment: .leading) {
+                            VStack(alignment: .leading) {
+                                Text("\(locality.name ?? "N/A")").fontWeight(.bold)
+                                HStack {
+                                    Text("Population: ")
+                                    Spacer()
+                                    Text("\(Image(systemName: "person.crop.circle")) \(locality.population)")
+                                }
+                                
+                                HStack {
+                                    Text("Population Density: ")
+                                    Spacer()
+                                    Text("\(Image(systemName: "rectangle.stack.person.crop")) \(String(format:"%.2f", locality.density))\n")
+                                }
+                                
+                                HStack {
+                                    Text("Estimated Threat Level: ")
+                                    Spacer()
+                                    Text("\(Image(systemName: "waveform.path.ecg")) \(String(format:"%.2f", locality.threatLevel ))")
+                                }
+                                
+                                HStack {
+                                    Text("\(locality.name ?? "N/A") Estimated Cases")
+                                    Spacer()
+                                    Text("\(Image(systemName: "cross.circle.fill")) \(locality.covidCases )")
+                                }
+                            }
+                        }
+                        
+                        Color.white.opacity(0.01)
+                            .onTapGesture {
+                            }
+                    }
+                }
+            }
+        }
+    }
 }
