bc57adb70e4a6a63fdcf4f8feafb587c9b2483af
yuan wang <wang85@sheridancollege.ca>
2020-12-07 12:35:23 -0500

    provinces view set up

---
+++ COVID-19 Hotspot Map/Views/ProvincesView.swift
@@ -0,0 +1,38 @@
+
+import SwiftUI
+
+struct ProvincesView: View {
+    @EnvironmentObject var covidViewModel: CovidViewModel
+    @EnvironmentObject var navigationHelper: NavigationHelper
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    private var localities: [String: Locality] = [String: Locality]()
+    private var provinces: [String] = [String]()
+    
+    init(localities: [String: Locality]) {
+        self.localities = localities
+        
+        for (_ , locacity) in localities {
+            if let province = locacity.province  {
+                if !provinces.contains(province) {
+                    provinces.append(province)
+                }
+            }
+        }
+    }
+    
+    var body: some View {
+
+        VStack{
+            List {
+                ForEach(self.provinces, id: \.self) { province in
+                    NavigationLink(destination: ProvinceCitiesView(localities: self.localities, selectedProvince: province)) {
+                        Text(province)
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
